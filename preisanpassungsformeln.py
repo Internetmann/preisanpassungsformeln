71c0dbccc1fee667e386400174ac7876ca181861
Internetmann <50077698+Internetmann@users.noreply.github.com>
2023-06-26 21:42:22 +0200

    Update preisanpassungsformeln.py

--- preisanpassungsformeln.py
+++ preisanpassungsformeln.py
@@ -65,7 +65,7 @@ if not error_message:
     fig.add_trace(go.Scatter(x=dates, y=gas_index2 / gas_index2_0 * 100, name='Erdgas, Börsennotierungen', line=dict(color='darkblue', width=2)))
 
     # Add the dummy trace for the secondary y-axis
-    fig.add_trace(go.Scatter(x=dates, y=arbeitspreis_neu / arbeitspreis_neu[0] * 100, name='Arbeitspreis (€/MWh)', line=dict(color='yellow', width=2), yaxis='y2'))
+    fig.add_trace(go.Scatter(x=dates, y=arbeitspreis_neu / arbeitspreis_neu[0] * 50, name='Arbeitspreis (€/MWh)', line=dict(color='yellow', width=2), yaxis='y2'))
 
     # Calculate the range for the secondary y-axis
     primary_y_range = [0, max(arbeitspreis_neu / arbeitspreis_neu[0] * 100)]
