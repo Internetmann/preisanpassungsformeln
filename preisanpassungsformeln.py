bd6bc57d992adda08490d56295d2925feb823030
Internetmann <50077698+Internetmann@users.noreply.github.com>
2023-06-26 22:06:17 +0200

    Update preisanpassungsformeln.py

--- preisanpassungsformeln.py
+++ preisanpassungsformeln.py
@@ -128,10 +128,10 @@ if not error_message:
 
 
     # Display the input parameters and the plot
-     if error_message:
-            st.error(error_message)
-     else:
-            st.plotly_chart(fig, use_container_width=True)
+    if error_message:
+        st.error(error_message)
+    else:
+        st.plotly_chart(fig, use_container_width=True)
 
 
 
