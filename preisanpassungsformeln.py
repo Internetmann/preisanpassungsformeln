3d149cbd4c457678cae93c910eb78e6fcc436b0b
Internetmann <50077698+Internetmann@users.noreply.github.com>
2023-06-26 17:49:23 +0200

    Update preisanpassungsformeln.py

--- preisanpassungsformeln.py
+++ preisanpassungsformeln.py
@@ -21,19 +21,19 @@ st.title('Fernwärme Arbeitspreisanpassung')
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    st.subheader('Fix-Element')
+    #st.subheader('Fix-Element')
     fix_element = st.number_input('Fix-Element', min_value=0.0, max_value=1.0, step=0.01, value=0.2)
 
 with col2:
-    st.subheader('Kostenelement')
+    #st.subheader('Kostenelement')
     kostenelement = st.number_input('Kostenelement', min_value=0.0, max_value=1.0, step=0.01, value=0.4)
 
 with col3:
-    st.subheader('Marktelement')
+    #st.subheader('Marktelement')
     marktelement = st.number_input('Marktelement', min_value=0.0, max_value=1.0, step=0.01, value=0.4)
 
 with col4:
-    st.subheader('Basis-Arbeitspreis')
+    #st.subheader('Basis-Arbeitspreis')
     basis_arbeitspreis = st.number_input('Basis-Arbeitspreis', min_value=20, max_value=200, step=1, value=50)
 
 # Ensure the elements sum up to 1
