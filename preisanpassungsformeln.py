c80452529da0b066286f229fa509c8d8d988b09e
Internetmann <50077698+Internetmann@users.noreply.github.com>
2023-06-26 20:38:27 +0200

    Update preisanpassungsformeln.py

--- preisanpassungsformeln.py
+++ preisanpassungsformeln.py
@@ -76,20 +76,7 @@ if not error_message:
 
     # Calculate the scale factor for the second y-axis
     scale_factor = max(arbeitspreis_neu_scaled) / max(arbeitspreis_neu_scaled)  # Use the maximum value of Arbeitspreis
-"""
-    # Configure the second y-axis (right)
-    fig.update_layout(
-        yaxis2=dict(
-            title='Arbeitspreis in €/MWh',
-            side='right',
-            overlaying='y',
-            showgrid=False,
-            tickfont=dict(color='red'),  # Set the tick labels to red
-            #range=[0, max(arbeitspreis_neu_scaled) * scale_factor]  # Set the range based on the scale factor
-            range=[0, max(arbeitspreis_neu)]
-        )
-    )
-"""
+
     # Assign the scaled Arbeitspreis data to the second y-axis
     #fig.add_trace(go.Scatter(x=dates, y=arbeitspreis_neu_scaled * scale_factor, name='Arbeitspreis (€/MWh)', line=dict(color='red', width=2), yaxis='y2'))
 
